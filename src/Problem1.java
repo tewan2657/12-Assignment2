8d81f5270da307df0c6df1672db72aae74d0eef5
tewan2657 <tewan2657@HRH-IC0027326.wrdsb.ca>
2017-09-14 10:11:15 -0400

    Still working on problem, starting problem 3.

--- src/Problem1.java
+++ src/Problem1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author tewan2657
@@ -15,37 +14,34 @@ public class Problem1 {
     /**
      * @param args the command line arguments
      */
-    public static int digitalSum(int n){
+    public static int digitalSum(int n) {
         //base Case
         // checking if the number is a single digit
 
-        if(n/10 ==0){ 
+        if (n / 10 == 0) {
             return n;
-        }else{
+        } else {
             for (int i = 0; i < n; i++) {
-               n= n%10;
-               
-               
+                i = n / 10;
+                n = i + n % 10;
+
+
             }
-              return n;
-         }
-         
+            return n;
         }
-        
-        
-    
-    
-    
+
+    }
+
     public static void main(String[] args) {
         // Create a Scanner 
         Scanner input = new Scanner(System.in);
-        
+
         //Ask the user to input a number 
         System.out.println("Input a number");
         int n = input.nextInt();
-        
+
         int sum = digitalSum(n);
         System.out.println(sum);
-        
+
     }
 }
---
+++ src/Problem3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tewan2657
+ */
+public class Problem3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int triangle(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
