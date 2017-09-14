dcaf71ee14063c7b445d446388a49c0a8a2be389
Adars <Adars@LAPTOP-5M1MF8QA>
2017-09-13 21:40:53 -0400

    Working on Problem 1.

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,10 +15,37 @@ public class Problem1 {
     /**
      * @param args the command line arguments
      */
+    public static int digitalSum(int n){
+        //base Case
+        // checking if the number is a single digit
+
+        if(n/10 ==0){ 
+            return n;
+        }else{
+            for (int i = 0; i < n; i++) {
+               n= n%10;
+               
+               
+            }
+              return n;
+         }
+         
+        }
+        
+        
     
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a Scanner 
+        Scanner input = new Scanner(System.in);
+        
+        //Ask the user to input a number 
+        System.out.println("Input a number");
+        int n = input.nextInt();
+        
+        int sum = digitalSum(n);
+        System.out.println(sum);
+        
     }
 }
